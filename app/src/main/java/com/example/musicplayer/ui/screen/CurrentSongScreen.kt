df8131fb4745de30efd1bd1700867aba301a78dc
PiotrCholewicki <piotr.cholewicki@gmail.com>
2025-06-09 01:21:15 +0200

    UI changes

--- app/src/main/java/com/example/musicplayer/ui/screen/CurrentSongScreen.kt
+++ app/src/main/java/com/example/musicplayer/ui/screen/CurrentSongScreen.kt
@@ -1,11 +1,13 @@
 package com.example.musicplayer.ui.screen
 
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
@@ -16,9 +18,13 @@ import coil.compose.AsyncImage
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.musicplayer.R
 import com.example.musicplayer.viewmodel.SongInfoViewModel
 import com.example.musicplayer.viewmodel.TrackViewModel
 import com.example.musicplayer.viewmodel.MusicPlayerViewModel
@@ -30,7 +36,6 @@ fun CurrentSong(
     songName: String,
     artist: String,
     musicPlayerViewModel: MusicPlayerViewModel,
-    trackViewModel: TrackViewModel,
     navController: NavController,
 
     ) {
@@ -39,45 +44,66 @@ fun CurrentSong(
         Column(
             modifier = Modifier
                 .padding(8.dp)
-                .verticalScroll(rememberScrollState()),
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
             //api display
+            val apiKey = stringResource(R.string.api_key)
             val viewModel: SongInfoViewModel = viewModel()
 
-            LaunchedEffect(songName, artist) {
+            LaunchedEffect(songName, artist, apiKey) {
                 viewModel.fetchSongInfo(
-                    apiKey = "2a1e7bb8dbb77494cb8334cd8607ab0a",
+                    apiKey = apiKey,
                     songName = songName,
                     artist = artist
 
                 )
             }
+
             val songInfo by viewModel.songInfo
             songInfo?.let { info ->
-                Row { Text("${info.artist?.name} - ${info.name}") }
+                Row(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        "${info.artist?.name} - ${info.name}",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
 
-                Spacer(modifier.padding(4.dp))
+                Row {
+                    Text("Genres: ", fontWeight = FontWeight.SemiBold)
+                    info.toptags?.tag?.forEach { tag ->
+                        if (tag.name != info.artist?.name) {
+                            Row { Text(text = "${tag.name} ") }
+                        } //displaying genres without duplicates with the band name
+                    }
+                }
+                Spacer(modifier.padding(8.dp))
 
-                val minutes = info.duration.toInt()/1000/60
-                val seconds = info.duration.toInt()/1000 - minutes * 60
-
-                Row {Text("Długość: ${minutes}:${seconds}\r\n")}
-
-
-                Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.padding(4.dp)){
+                Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.padding(4.dp)) {
                     AsyncImage(
                         model = info.album?.image?.get(3)?.url,
                         contentDescription = "Album cover",
-                        modifier = modifier.fillMaxSize()
+                        modifier = modifier
+                            .fillMaxSize()
+                            .size(240.dp)
                     )
                 }
-                Row{
-                    info.toptags?.tag?.forEach { tag -> Row{Text(text = "${tag.name} ")} } //displaying genres
 
+                Spacer(modifier.padding(8.dp))
+
+                val minutes = info.duration.toInt() / 1000 / 60
+                val seconds = info.duration.toInt() / 1000 - minutes * 60
+
+                Row {
+                    Text("Length: ", fontWeight = FontWeight.SemiBold )
+                    Text("${minutes}:${seconds}")
                 }
+
+
             }
             Spacer(modifier = Modifier.weight(1f))
             Row { Footer(musicPlayerViewModel, navController) }
--- app/src/main/java/com/example/musicplayer/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/musicplayer/ui/screen/MainScreen.kt
@@ -1,13 +1,14 @@
 package com.example.musicplayer.ui.screen
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -25,7 +26,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.musicplayer.R
 import com.example.musicplayer.viewmodel.MusicPlayerViewModel
@@ -41,37 +44,39 @@ fun MainScreen(
 
 ) {
 
-    Surface {
+    Surface(modifier = modifier.fillMaxSize()) {
         Column(
-            modifier = modifier.padding(8.dp) // główny kontener
+            modifier = modifier.padding(8.dp).fillMaxSize() // główny kontener
         ) {
-            // TOP BAR
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(40.dp)
-                    .padding(4.dp),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text("to_do: RASPBERRY")
-            }
 
             // MIDDLE BAR
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .weight(1f)
-                    .padding(4.dp)
-            ) {
-                Text(stringResource(R.string.list_of_audio_tracks))
+                    .padding(8.dp),
 
-                FilesDisplay(
-                    modifier = Modifier.weight(1f),
-                    navController = navController,
-                    musicPlayerViewModel = viewModel,
-                    trackViewModel = trackViewModel
-                )
+                ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = stringResource(R.string.list_of_audio_tracks),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                Row {
+                    FilesDisplay(
+                        modifier = Modifier.weight(1f),
+                        navController = navController,
+                        musicPlayerViewModel = viewModel,
+                        trackViewModel = trackViewModel
+                    )
+                }
+
             }
         }
     }
@@ -91,10 +96,10 @@ fun Footer(musicPlayerViewModel: MusicPlayerViewModel, navController: NavControl
             .padding(8.dp)
 
     ) {
-
         Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
+            modifier = Modifier.fillMaxWidth().padding(4.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Button(onClick = { musicPlayerViewModel.togglePlayPause() }) {
                 if (isPlaying) {
@@ -110,6 +115,7 @@ fun Footer(musicPlayerViewModel: MusicPlayerViewModel, navController: NavControl
                     )
                 }
             }
+            Spacer(modifier = Modifier.width(8.dp))
             Button(onClick = {
                 musicPlayerViewModel.stopPlayback()
                 navController.navigate("MainScreen")
@@ -120,17 +126,20 @@ fun Footer(musicPlayerViewModel: MusicPlayerViewModel, navController: NavControl
                 )
             }
         }
+        Row(){
+            Slider(
+                value = if (totalDuration > 0) currentPosition.toFloat() / totalDuration else 0f,
+                onValueChange = {
+                    val newPos = (it * totalDuration).toInt()
+                    musicPlayerViewModel.seekTo(newPos)
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
 
-        Slider(
-            value = if (totalDuration > 0) currentPosition.toFloat() / totalDuration else 0f,
-            onValueChange = {
-                val newPos = (it * totalDuration).toInt()
-                musicPlayerViewModel.seekTo(newPos)
-            },
-            modifier = Modifier.fillMaxWidth()
-        )
     }
 }
+
 @Composable
 fun FilesDisplay(
     modifier: Modifier = Modifier,
@@ -140,7 +149,6 @@ fun FilesDisplay(
 ) {
 
     val currentFile by musicPlayerViewModel.currentFile.collectAsState()
-
 
     Column(
         modifier = modifier
@@ -152,10 +160,13 @@ fun FilesDisplay(
         tracks.forEach { track ->
             val trackTitle = track.name
             val trackArtist = track.artist
-
+            val currentTrack = track.path
             TextButton(
                 onClick = {
-                    musicPlayerViewModel.playFile(track.path)
+                    if(currentFile != currentTrack){ //dont restart the player when clicked the same song
+                        musicPlayerViewModel.playFile(track.path)
+                        Log.e("MainScreen", "$currentFile, $currentTrack")
+                    }
                     navController.navigate("CurrentSong/${track.id}")
                 },
                 modifier = Modifier
